987d650c11b2efbfeb23faf420c5f2371677810a
Jamila BOUMEHDI <mabchourjamila87@gmail.com>
2024-03-20 14:19:11 +0100

    ajouter les fonts google en local

--- wp-content/themes/chicdressing/functions.php
+++ wp-content/themes/chicdressing/functions.php
@@ -5,9 +5,13 @@ function chicdressing_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
 }
 
-add_filter( 'big_image_size_threshold', '__return_false' );
+// cette fonction permet de décharger manuellement les fonts google du thème parent (ashe)
+function deregister_parent_google_fonts() {
+    // Désenregistrer les feuilles de style pour toute la session
+    wp_deregister_style( 'ashe-playfair-font');
+    wp_deregister_style( 'ashe-opensans-font');
+    wp_deregister_style( 'ashe-kalam-font');
+    //wp_deregister_style( 'ashe-rokkitt-font');
+}
 
-
-
-
- 
+add_action('wp_enqueue_scripts', 'deregister_parent_google_fonts', 100);
